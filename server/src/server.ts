9b1c5298eee44a991689642fbff9c0b72a402b65
TakaGui <gui.yukio175@gmail.com>
2020-08-04 17:16:23 -0300

    refactor: use routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.status(200).json({ message: 'Hello World '});
-});
+app.use(routes);
 
 app.listen(3333);
