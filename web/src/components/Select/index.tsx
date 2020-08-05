f44ddf6bcbeb959337d1c857176fd67addbb2458
TakaGui <gui.yukio175@gmail.com>
2020-08-05 14:14:22 -0300

    fix: add default value tag select

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -16,8 +16,8 @@ const Select: React.FC<ISelectProps> = ({ name, label, options, ...rest }) => {
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
       
-      <select id={name} {...rest}>
-        <option value="" disabled selected hidden>Selecione uma opção</option>
+      <select defaultValue="" id={name} {...rest}>
+        <option value="" disabled hidden>Selecione uma opção</option>
 
         {options.map(option => {
           return <option key={option.value} value={option.value}>{option.label}</option>
