d2bf3196e2a44a3d9f86110b2e6349ea063dcdd1
TakaGui <gui.yukio175@gmail.com>
2020-08-06 13:56:51 -0300

    add: create structure landing page

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import landingImg from '../../assets/images/landing.png';
+
+import styles from './styles';
+
+function Landing() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.banner} source={landingImg} />
+
+      <Text style={styles.title}>
+        Seja bem-vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+    </View>
+  );
+}
+
+export default Landing;
