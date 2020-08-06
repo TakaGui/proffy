bab7cc6d749f4cfa2af2a07318465f2950641281
TakaGui <gui.yukio175@gmail.com>
2020-08-06 15:57:34 -0300

    add: create structure favorites teacher tab

---
+++ mobile/src/pages/FavoritesTeacher/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function FavoritesTeacher() {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default FavoritesTeacher;
