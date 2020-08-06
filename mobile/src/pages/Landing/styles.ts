b6f18969f91266b3555434031749dc51465d25bf
TakaGui <gui.yukio175@gmail.com>
2020-08-06 14:29:34 -0300

    add: styles button and total connections

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
   },
 
   title: {
+    fontFamily: 'Poppins_400Regular',
     color: '#FFF',
     fontSize: 20,
     lineHeight: 30,
@@ -22,7 +23,44 @@ const styles = StyleSheet.create({
   },
 
   titleBold: {
-    fontWeight: 'bold',
+    fontFamily: 'Poppins_600SemiBold',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between',
+  },
+
+  button: {
+    height: 150,
+    width: '48%',
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: 'space-between',
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871F5',
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04D361',
+  },
+
+  buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 20,
+  },
+
+  totalConnections: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#D4C2FF',
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+    marginTop: 40,
   },
 });
 
